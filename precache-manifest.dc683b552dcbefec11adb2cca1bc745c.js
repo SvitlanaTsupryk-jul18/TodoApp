70b833549330f3f7150374f3acac6b521dbf69cc
Svet <stsupryk@gmail.com>
2019-06-14 21:56:50 +0300

    Updates

---
+++ precache-manifest.dc683b552dcbefec11adb2cca1bc745c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c81c127ec211869a2651233f57c07b",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "728fb5d0b099cc3be171",
+    "url": "/TodoApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/TodoApp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "728fb5d0b099cc3be171",
+    "url": "/TodoApp/static/js/main.a5ff0cc0.chunk.js"
+  },
+  {
+    "revision": "b429d889341aa25a2548",
+    "url": "/TodoApp/static/js/runtime~main.5a342d52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
